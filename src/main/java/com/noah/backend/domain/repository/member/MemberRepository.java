0b2cf1a29fb45ecc2c99b26af7ba5bb0c8dc7822
aroDaddy <th.jeong830@gmail.com>
2023-08-28 11:11:25 +0900

    회원 레포지토리 생성

---
+++ src/main/java/com/noah/backend/domain/repository/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.noah.backend.domain.repository.member;
+
+import com.noah.backend.domain.entity.member.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+}
