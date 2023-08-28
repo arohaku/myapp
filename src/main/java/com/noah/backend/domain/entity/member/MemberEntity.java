5ab5f386feef62c3c15d808ca1611f4c8f090243
aroDaddy <th.jeong830@gmail.com>
2023-08-28 11:09:37 +0900

    회원 엔티티 생성

---
+++ src/main/java/com/noah/backend/domain/entity/member/MemberEntity.java
@@ -0,0 +1,33 @@
+package com.noah.backend.domain.entity.member;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "MEMBER")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @NotEmpty
+    @Email(message = "유효하지 않은 이메일 형식입니다.",
+            regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    private String email;
+
+    @NotEmpty
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
+    private String password;
+
+    @NotEmpty
+    private String nickname;
+}
