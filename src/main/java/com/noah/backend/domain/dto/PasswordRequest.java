a51d8ad6ee38bf5f14d495dfdf17d49c09428b69
aroDaddy <th.jeong830@gmail.com>
2023-08-28 15:31:29 +0900

    비밀번호dto 생성

---
+++ src/main/java/com/noah/backend/domain/dto/PasswordRequest.java
@@ -0,0 +1,21 @@
+package com.noah.backend.domain.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class PasswordRequest {
+
+    @NotEmpty
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
+    private final String oldPassword;
+
+    @NotEmpty
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
+    private final String newPassword;
+}
