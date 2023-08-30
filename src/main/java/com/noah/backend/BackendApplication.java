458ae5ee9ae1f1be49fde82e3426b6aad82b1eea
aroDaddy <th.jeong830@gmail.com>
2023-08-30 11:58:12 +0900

    Redis 캐시 설정 변경

--- src/main/java/com/noah/backend/BackendApplication.java
+++ src/main/java/com/noah/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableCaching
 @EnableRedisHttpSession
 public class BackendApplication {
 
