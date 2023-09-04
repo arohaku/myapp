3525de9a61ddad5336509c24c0e977c1ae92b00d
aroDaddy <th.jeong830@gmail.com>
2023-09-04 13:46:25 +0900

    사용자카테고리 기준으로 거래 목록 조회 기능 구현

--- src/main/java/com/noah/backend/post/controller/PostSearchController.java
+++ src/main/java/com/noah/backend/post/controller/PostSearchController.java
@@ -7,11 +7,13 @@ import com.noah.backend.post.dto.AddressRequest;
 import com.noah.backend.post.dto.PostPageResponse;
 import com.noah.backend.post.service.TradePostSearchService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,6 +38,16 @@ public class PostSearchController {
     public ResponseEntity<PostPageResponse> getTradePostsByAddress(@Valid AddressRequest address, Pageable pageable) {
 
         PostPageResponse page = tradePostSearchService.findAllByAddress(address, pageable);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @LoginRequired
+    @GetMapping("/categories")
+    public ResponseEntity<PostPageResponse> getTradePostsByCategory(@RequestParam("category") @NotEmpty String category,
+                                                                    @LoginMember Member member, Pageable pageable) {
+
+        PostPageResponse page = tradePostSearchService.findALlByCategory(category, member, pageable);
 
         return ResponseEntity.ok(page);
     }
